55730b88a51f6e34036144ec77ed0661ea27e9d0
Zal Machado <zalmachado@gmail.com>
2016-07-18 15:18:28 -0700

    initial commit

---
+++ PRTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PRTestApp
+//
+//  Created by Zal Machado on 2016-07-18.
+//  Copyright © 2016 Zal Machado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PRTestApp/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  PRTestApp
+//
+//  Created by Zal Machado on 2016-07-18.
+//  Copyright © 2016 Zal Machado. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonPress:(id)sender {
+    [self performSelector:@selector(die_die)];
+}
+
+@end
